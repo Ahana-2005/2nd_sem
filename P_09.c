0eca1c01e4f333b7f3df2ba7ec0240de96ba85e6
Ahana-2005 <ahana25052005@gmail.com>
2024-08-19 21:08:41 +0530

    Update P_09.c

--- P_09.c
+++ P_09.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 int main()
 {
-    int i,n,pos,element,found = 0;
+    int n,pos,element,found = 0;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
     int arr[n];
     printf("Enter the elements of the array: \n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("Enter the element of the array in index %d:",i);
         scanf("%d",&arr[i]);
     }
     printf("Enter the value of the element to be deleted:");
     scanf("%d", &element);
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == element)
         {
@@ -25,12 +25,12 @@ int main()
     }
     if (found == 1)
     {
-        for (i = pos; i < n - 1; i++)
+        for (int i = pos; i < n - 1; i++)
         {
             arr[i] = arr[i + 1];
         }
         printf("The array after deleting the element is: \n");
-        for (i = 0; i < n - 1; i++)
+        for (int i = 0; i < n - 1; i++)
         {
             printf("%d ", arr[i]);
         }
